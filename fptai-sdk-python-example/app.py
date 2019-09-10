35880025fc2e822383ed92ec7a08f782b17d2592
tuantk <kimtuan1102@gmail.com>
2019-09-10 16:53:27 +0700

    Update Example

--- fptai-sdk-python-example/app.py
+++ fptai-sdk-python-example/app.py
@@ -1,5 +1,10 @@
+import json
+
+import flask
 from flask import Flask, request
 from flask_restful import Resource, Api
+from fptai_chatbot_sdk import constant
+from fptai_chatbot_sdk.manage import BotManager
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,11 +12,35 @@ api = Api(app)
 
 class BotResponse(Resource):
     def get(self):
-        return '62b1de06-d2f8-11e9-bb65-2a2ae2dbcce4'
+        response = flask.make_response("62b1de06-d2f8-11e9-bb65-2a2ae2dbcce4")
+        response.headers['content-type'] = 'application/octet-stream'
+        return response
 
     def post(self):
-        print(request.json)
-        return 'a'
+        # Bot Response
+
+        # Bot Manager
+        # Bot Response Object
+        bot_response_obj = BotManager.parse_response(request.json)
+        messages = bot_response_obj.messages
+        for message in messages:
+            type = message.type
+            if type == constant.TEXT_MESSAGE:
+                # Text message obj
+                text_message = message.get_as_text_message()
+            elif type == constant.IMAGE_MESSAGE:
+                # Text image obj
+                image_message = message.get_as_image_message()
+            elif type == constant.CAROUSEL_MESSAGE:
+                # Text message obj
+                carousel_message = message.get_as_carousel_message()
+            elif type == constant.QUICK_REPLY_MESSAGE:
+                # Text message obj
+                quick_reply_message = message.get_as_quick_reply_message()
+            else:
+                # Text message obj
+                form_message = message.get_as_form_message()
+        return "nothing"
 
 api.add_resource(BotResponse, '/receive-response')
 if __name__ == '__main__':
--- fptai-sdk-python-example/example.py
+++ fptai-sdk-python-example/example.py
@@ -1,2 +1,30 @@
+from fptai_chatbot_sdk import constant
+from fptai_chatbot_sdk.manage import BotManager
+
+
 class Example():
-    print("a")
+    bot_host = "https://dev-local-v3.fpt.ai"
+    bot_token = "2b0c6cf6ced17d46791c20d4dc76ef1c"
+    channel = constant.CHANNEL_API
+    bot_manager = BotManager(bot_token, channel, bot_host)
+
+    # SEND PAYLOAD MESSAGE
+    # Set variable name = chat bot
+    attributes = {
+        "name": "chatbot"
+    }
+    # Step name
+    step_name = "NextStep"
+
+    # Sender Id
+    sender_id = "5d7d5b30acb2a068698aeb2ed7176075"
+    res = bot_manager.build_payload_message(step_name,attributes).send_message(sender_id)
+    # Response success or failed
+    print(res)
+
+    # SEND TEXT MESSAGE
+    # content
+    content = "hi"
+    sender_id = "5d7d5b30acb2a068698aeb2ed7176075"
+    res = bot_manager.build_text_message(content).send_message(sender_id)
+    print(res)
